1252c2bf22d818ce4d3aa69a0d277faefbf6c9a7
khanglv-vuatho <khanglv@vuatho.com>
2024-01-09 22:53:05 +0700

    donee

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -6,21 +6,20 @@ import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 import { NextUIProvider } from '@nextui-org/react'
 import { Provider } from 'react-redux'
 
-
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     // <Provider store={store}>
-      <NextUIProvider>
-        <NextThemesProvider attribute='class' defaultTheme='light'>
-          <ProgressBar
-            height='4px'
-            color='#3748A0'
-            options={{ showSpinner: true }}
-            shallowRouting
-          />
-          {children}
-        </NextThemesProvider>
-      </NextUIProvider>
+    <NextUIProvider>
+      <NextThemesProvider attribute='class' defaultTheme='light'>
+        <ProgressBar
+          height='4px'
+          color='#3748A0'
+          options={{ showSpinner: true }}
+          shallowRouting
+        />
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
     // </Provider>
   )
 }
