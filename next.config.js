50118de44e1adfb6a12dbc99feaab376282594b5
khanglv-vuatho <khanglv@vuatho.com>
2024-01-09 22:07:37 +0700

    init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withNextIntl = require('next-intl/plugin')();
 
-module.exports = nextConfig
+module.exports = withNextIntl({
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'flagcdn.com' },
+      { protocol: 'https', hostname: 'vuatho.com' },
+      { protocol: 'https', hostname: 'cdn.vuatho.com' },
+      { protocol: 'https', hostname: 'icdn.vuatho.com' },
+      { protocol: 'https', hostname: 'api.vuatho.com' },
+      { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'https', hostname: 'sandbox-api-cms.vuatho.com' },
+      { protocol: 'http', hostname: '192.168.1.72' },
+    ],
+  },
+});
